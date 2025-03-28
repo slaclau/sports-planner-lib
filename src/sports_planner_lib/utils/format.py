b965474449062bfcc8b1bffcebd8f810602e1ff4
Sebastien Laclau <seb.laclau@gmail.com>
2025-03-28 10:22:43 +0000

    fix: allow time formatter to hide hours

--- src/sports_planner_lib/utils/format.py
+++ src/sports_planner_lib/utils/format.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 
-def time(seconds, fractional=False):
-    hours = int(np.floor(seconds / 3600))
+def time(seconds, fractional=False, target="hours"):
+    if target == "hours":
+        hours = int(np.floor(seconds / 3600))
+    else:
+        hours = 0
     seconds = seconds - 3600 * hours
     mins = int(np.floor(seconds / 60))
     secs = int(seconds - 60 * mins)
@@ -10,7 +13,10 @@ def time(seconds, fractional=False):
     if hours > 0:
         rtn += f"{hours:d}:"
     if mins > 0 or (hours > 0 and mins >= 0):
-        rtn += f"{mins:0>2d}:"
+        if hours > 0:
+            rtn += f"{mins:0>2d}:"
+        else:
+            rtn += f"{mins:d}:"
     if secs >= 0:
         rtn += f"{secs:0>2d}"
     if fractional:
